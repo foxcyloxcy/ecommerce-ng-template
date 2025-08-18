531571edbf5a82d123e3dea94a6ae1dc15b4faf1
some3L <luckyllemos0909@gmail.com>
2025-08-18 21:59:38 +0800

    Update routing with lazy loading

--- src/app/features/checkout/checkout-routing-module.ts
+++ src/app/features/checkout/checkout-routing-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: CheckoutComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CheckoutRoutingModule { }
+export class CheckoutRoutingModule {}
