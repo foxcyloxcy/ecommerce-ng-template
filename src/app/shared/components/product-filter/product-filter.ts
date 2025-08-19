dcf6f5fc228933f83b26c11a5d19e89a947fa6d1
some3L <luckyllemos0909@gmail.com>
2025-08-19 18:44:55 +0800

    Initial commit

---
+++ src/app/shared/components/product-filter/product-filter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-filter',
+  imports: [],
+  templateUrl: './product-filter.html',
+  styleUrl: './product-filter.scss'
+})
+export class ProductFilter {
+
+}
